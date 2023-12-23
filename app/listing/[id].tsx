acea4d753fc7ab584eb853f622d5220c12ea822c
tamani phiri <tamanigabriel0@gmail.com>
2023-12-23 20:00:31 +0200

    c

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,6 +1,9 @@
+import { useLocalSearchParams } from 'expo-router'
 import { View, Text } from 'react-native'
 
 const Page = () => {
+    const {id}=useLocalSearchParams();
+    console.log(id);
   return (
     <View>
       <Text>Listing</Text>
