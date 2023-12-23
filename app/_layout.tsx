cf3b2990f9b1daf01997f25753012a5a5f578a17
tamani phiri <tamanigabriel0@gmail.com>
2023-12-23 18:13:19 +0200

    c

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,8 +20,10 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+    "mon":require("../assets/fonts/Montserrat-Regular.ttf"),
+    "mon-sb":require("../assets/fonts/Montserrat-SemiBold.ttf"),
+    "mon-b":require("../assets/fonts/Montserrat-Bold.ttf"),
+    "mon-eb":require("../assets/fonts/Montserrat-ExtraBold.ttf")
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
