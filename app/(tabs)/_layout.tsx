38fc8af1284e89d238c42df19e45c3eb041c0f00
tamani phiri <tamanigabriel0@gmail.com>
2023-12-23 18:57:08 +0200

    c

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,36 @@
 import { Tabs } from "expo-router";
 import Colors from "../../constants/Colors";
+import { AntDesign } from "@expo/vector-icons";
 
 const Layout = () => {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: Colors.yellow ,tabBarLabelStyle:{
-      fontFamily:"mon-sb"
-    }}}>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors.yellow,
+        tabBarLabelStyle: {
+          fontFamily: "mon-sb",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
-        options={{ tabBarLabel: "Explore", title: "Explore" }}
+        options={{
+          tabBarLabel: "Explore",
+          title: "Explore",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="search1" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="wishlists"
+        options={{
+          tabBarLabel: "Wishlists",
+          title: "Wishlists",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="heart" size={size} color={color} />
+          ),
+        }}
       />
     </Tabs>
   );
--- app/(tabs)/wishlishts.tsx
+++ app/(tabs)/wishlishts.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Page = () => {
   return (
     <View>
-      <Text>Wishlist</Text>
+      <Text>Wishlists</Text>
     </View>
   )
 }
