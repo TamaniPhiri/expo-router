386af1f9bb42cc9da647ee1b5c1eadf59f3184fc
tamani phiri <tamanigabriel0@gmail.com>
2023-12-23 18:41:24 +0200

    c

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,7 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
-
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+  dark: "#202C39",
+  semiDark: "#283845",
+  mid: "#B8B08D",
+  yellow: "#F2D492",
+  orange: "#F29559",
 };
