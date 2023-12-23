7b253653dfb07aafa325386cdc668d4bcd429db0
tamani phiri <tamanigabriel0@gmail.com>
2023-12-23 19:59:33 +0200

    c

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,11 +1,9 @@
 import { View, Text } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
 
 const Page = () => {
   return (
     <View>
-      <Link href={"/(modals)/booking"}>Book</Link>
+      <Text>Listing</Text>
     </View>
   )
 }
