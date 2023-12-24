af4c350a287eb06645bb9debff637332a1808826
tamani phiri <tamanigabriel0@gmail.com>
2023-12-25 00:08:48 +0200

    c

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Stack } from "expo-router";
 import ExploreHeader from "../../components/ExploreHeader";
 import Listings from "../../components/Listings";
 
 const Page = () => {
+  const [category, setCategory] = useState<string>("Tiny homes");
   const onDataChanged = (category: string) => {
-    console.log(category);
+    setCategory(category);
   };
   return (
     <View style={{ flex: 1 }}>
