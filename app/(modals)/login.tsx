370ff96e6bb77880b1940d878ba8167573135c8a
tamani phiri <tamanigabriel0@gmail.com>
2023-12-24 21:39:50 +0200

    c

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -17,6 +17,7 @@ const styles = StyleSheet.create({
     borderColor: Colors.mid,
     borderWidth: 1,
     padding:8,
-    borderRadius:6
+    borderRadius:6,
+    color:"white"
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,7 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { SplashScreen, Stack, router } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
-import { useColorScheme } from "react-native";
-
-
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -51,9 +43,8 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DarkTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen
