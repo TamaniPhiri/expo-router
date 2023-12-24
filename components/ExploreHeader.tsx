d3f5b7a6c918f96fb0223b55b53bf35ab6d0ba9c
tamani phiri <tamanigabriel0@gmail.com>
2023-12-24 22:23:30 +0200

    c

--- components/ExploreHeader.tsx
+++ components/ExploreHeader.tsx
@@ -1,12 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+
+const categories = [
+  {
+    name: "Tiny homes",
+    icon: "home",
+  },
+  {
+    name: "Cabins",
+    icon: "house-siding",
+  },
+  {
+    name: "Trending",
+    icon: "local-fire-department",
+  },
+  {
+    name: "Play",
+    icon: "videogame-asset",
+  },
+  {
+    name: "City",
+    icon: "apartment",
+  },
+  {
+    name: "Beachfront",
+    icon: "beach-access",
+  },
+  {
+    name: "Countryside",
+    icon: "nature-people",
+  },
+];
 
 const ExploreHeader = () => {
   return (
     <View>
-      <Text style={{color:"white"}}>ExploreHeader</Text>
+      <Text style={{ color: "white" }}>ExploreHeader</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ExploreHeader
+export default ExploreHeader;
