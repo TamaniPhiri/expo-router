f4e751cc96c11254f6869369e15859084301f65b
tamani phiri <tamanigabriel0@gmail.com>
2023-12-25 15:31:04 +0200

    c

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -2,6 +2,7 @@ import { useLocalSearchParams } from "expo-router";
 import { View, Text, StyleSheet, Dimensions } from "react-native";
 import listingsData from "../../assets/data/airbnb-listings.json";
 import Animated, { FadeInLeft, ZoomInUp } from "react-native-reanimated";
+import { Feather } from "@expo/vector-icons";
 
 const { width } = Dimensions.get("window");
 const Page = () => {
@@ -32,7 +33,12 @@ const Page = () => {
             {listing.room_type} in {listing.smart_location}
           </Text>
           <Animated.View style={{ borderColor: "gray", borderWidth: 0.5 }}>
-            {listing.guests_included} guests · {listing.bedrooms} bedrooms
+            <View>
+              <Feather name="users"/>
+              <Text>
+              {listing.guests_included}
+              </Text>
+              </View> {listing.bedrooms} 
           </Animated.View>
         </View>
       </Animated.ScrollView>
