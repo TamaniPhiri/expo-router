3d5e7361c15e1f85795271a7f9a994809bf8f81b
tamani phiri <tamanigabriel0@gmail.com>
2023-12-25 17:34:09 +0200

    c

--- components/ListingsMap.tsx
+++ components/ListingsMap.tsx
@@ -1,4 +1,6 @@
 import { View, Text } from 'react-native'
+import MapView from 'react-native-maps'
+import listingsDataGeo from "../assets/data/airbnb-listings.geo.json";
 const ListingsMap = () => {
   return (
     <View>
