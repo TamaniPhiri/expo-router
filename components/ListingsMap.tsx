4017459d479722785f3f90fb6cd1daa9c6b61532
tamani phiri <tamanigabriel0@gmail.com>
2023-12-25 17:51:14 +0200

    c

--- components/ListingsMap.tsx
+++ components/ListingsMap.tsx
@@ -1,19 +1,29 @@
-import { View, Text } from 'react-native'
-import MapView from 'react-native-maps'
+import { View, Text, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 import listingsDataGeo from "../assets/data/airbnb-listings.geo.json";
-import AirbnbGeoListing from '../types/interface'
+import AirbnbGeoListing from "../types/interface";
 
-interface Props{
-  listings:AirbnbGeoListing;
+interface Props {
+  listings: AirbnbGeoListing;
 }
-const ListingsMap = ({listings}:Props) => {
+const ListingsMap = ({ listings }: Props) => {
   console.log(listings.geolocation);
-  
-  return (
-    <View>
-      <Text>ListingsMap</Text>
-    </View>
-  )
-}
 
-export default ListingsMap
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
+  );
+};
+
+export default ListingsMap;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    height: "100%",
+    width: "100%",
+  },
+});
