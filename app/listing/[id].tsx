d1d8e1ce2bef4da92605690538297d895303376c
tamani phiri <tamanigabriel0@gmail.com>
2023-12-25 14:56:58 +0200

    c

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,7 +1,7 @@
 import { useLocalSearchParams } from "expo-router";
 import { View, Text, StyleSheet, Dimensions } from "react-native";
 import listingsData from "../../assets/data/airbnb-listings.json";
-import Animated, { SlideInUp, SlideOutUp } from "react-native-reanimated";
+import Animated, { ZoomInUp} from "react-native-reanimated";
 
 const { width } = Dimensions.get("window");
 const Page = () => {
@@ -13,8 +13,7 @@ const Page = () => {
     <View style={styles.container}>
       <Animated.ScrollView>
         <Animated.Image
-          entering={SlideInUp}
-          exiting={SlideOutUp}
+          entering={ZoomInUp}
           source={{ uri: listing.xl_picture_url }}
           style={styles.image}
         />
