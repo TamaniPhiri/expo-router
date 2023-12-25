de7efd9117efcb8231934ffbb736ac544147fc04
tamani phiri <tamanigabriel0@gmail.com>
2023-12-25 17:13:56 +0200

    c

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -41,7 +41,7 @@ const Page = () => {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
-        <View style={{ flexDirection: "row", gap: 12 }}>
+        <View style={{ flexDirection: "row", gap: 12 ,alignItems:"center"}}>
           <TouchableOpacity
             style={{ backgroundColor: "white", borderRadius: 30, padding: 6 }}
             onPress={shareListing}
